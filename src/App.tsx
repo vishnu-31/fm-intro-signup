79dd7acc73ec43dd669189fd972ab480b135352c
vishnu-31 <vishnuvicky3636@gmail.com>
2023-12-08 17:38:48 +0530

    completed the challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import Form from "./Form"
 
+
+
 function App() {
   
   return (
-    <main className="flex flex-col w-[360px] md:w-[750px] md:flex-row text-center text-white m-4">
-      <div className="flex flex-col">
-        <h1 className="text-2xl  font-extrabold m-5">Learn to code by watching&nbsp;others</h1>
-        <p className="m-7">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
+    <main className="flex flex-col w-[375px] md:w-[1300px] md:flex-row text-center mt-32 text-white mx-4  ">
+      <div className="flex flex-col md:w-[650px] md:justify-center md:text-left pl-0">
+        <h1 className="text-2xl md:text-5xl font-extrabold m-5">Learn to code by watching&nbsp;others</h1>
+        <p className="m-7 md:text-lg font-medium">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.</p>
       </div>
-      <div className="flex-col">
-        <div className=" flex flex-col md:flex-row bg-purple-400 rounded-lg py-3 px-6 my-5 mx-2 shadow-lg">
-          Try it free 7 days then $20/mo.&nbsp;thereafter
+      <div className="flex-col w-full md:w-[50%] p-8">
+        <div className=" banner shadow-div flex flex-col md:flex-row rounded-lg py-5 px-6 md:justify-center my-5 mx-2 ">
+         <p> <a className="font-bold"> Try it free 7 days</a> then $20/mo.&nbsp;thereafter </p>
         </div>
-        <div className="bg-white p-4 m-2 rounded-lg">
+        <div className="shadow-div bg-white p-4 my-2 mx-0 rounded-lg">
           <Form/>
-          <div className="text-gray-400 text-xs px-8">
+          <div className="text-gray-400 text-xs px-5">
             By clicking the button, you are agreeing to our <a className="text-red-400 font-bold">Terms and Services</a>
           </div>
         </div>
--- src/Form.tsx
+++ src/Form.tsx
@@ -1,29 +1,101 @@
-
+import { useState } from "react";
+import EmailValidator from "email-validator"
 
 const Form =() => {
+    const [fnErr,setFnErr] = useState<boolean>(false);
+    const [lnErr,setLnErr] = useState<boolean>(false);
+    const [mErr,setMErr] = useState<string>("");
+    const [pErr,setPErr] = useState<boolean>(false);
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [mail,setMail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const validateFn =() =>{
+        if(firstName == ""){
+            setFnErr(true)
+        }else{
+            setFnErr(false)
+        }
+        if(lastName == ""){
+            setLnErr(true)
+        }else{
+            setLnErr(false)
+        }
+        if(mail == ""){
+            setMErr("empty")
+        }else if(!EmailValidator.validate(mail)){
+            setMErr("valid")
+        }else{
+            setMErr("")
+        }
+        if(password == ""){
+            setPErr(true)
+        }else{
+            setPErr(false)
+        }
+    }
+
     return(
-        <form className="text-md mx-2 my-4">
-            <input 
-                className="w-full h-16 my-2 border-2 rounded p-3" 
-                type="text"     
-                name="firstname" 
-                placeholder="First Name"/>
-            <input 
-                className="w-full h-16 my-2 border-2 rounded p-3" 
-                type="text"     
-                name="lastname"  
-                placeholder="Last Name"/>
-            <input 
-                className="w-full h-16 my-2 border-2 rounded p-3" 
-                type="email"    
-                name="mail"      
-                placeholder="Email address"/>
-            <input 
-                className="w-full h-16 my-2 border-2 rounded p-3" 
-                type="password" 
-                name="password"  
-                placeholder="Password"/>
-            <button className=" my-2 rounded-lg bg-green-500 p-3 w-full" type="button">Claim your free trail</button>
+        <form className="text-md font-bold text-black mx-2 my-2">
+            <div className="flex flex-col w-full">
+                <div className="my-4 pr-2 flex  flex-nowrap border-2  border-gray-300 h-16 rounded-lg">
+                    <input 
+                            className="w-full h-full text-gray-700 rounded-lg p-3 pl-8 border-0" 
+                            type={"string"}     
+                            name={"firstName"} 
+                            value={firstName}
+                            onChange={e=>setFirstName(e.target.value)}
+                            placeholder={"First Name"} formNoValidate/>
+                    { fnErr && <div className="flex w-[10%] items-center"><img  src="/images/icon-error.svg" alt="Error Icon" /></div> }   
+                </div>
+                {fnErr && <div className="italic font-normal text-right pr-5 text-xs text-red-500 " >First Name cannot be empty</div>}
+            </div> 
+            <div className="flex flex-col w-full">
+                <div className="my-4 pr-2 flex  flex-nowrap border-2  border-gray-300 h-16 rounded-lg">
+                    <input 
+                            className="w-full h-full text-gray-700 rounded-lg p-3 pl-8 border-0" 
+                            type={"string"}     
+                            name={"lastName"} 
+                            value={lastName}
+                            onChange={e=>setLastName(e.target.value)}
+                            placeholder={"Last Name"} formNoValidate/>
+                    {lnErr && <div className=" flex w-[10%] items-center"><img  src="/images/icon-error.svg" alt="Error Icon" /></div>    }
+                </div>
+                {lnErr &&  <div className="italic font-normal text-right pr-5 text-xs text-red-500 " >Last Name cannot be empty</div>}
+            </div> 
+            <div className="flex flex-col w-full">
+                <div className="my-4 pr-2 flex  flex-nowrap border-2  border-gray-300 h-16 rounded-lg">
+                    <input 
+                            className="w-full h-full text-gray-700 rounded-lg p-3 pl-8 border-0 focus:border-0" 
+                            type={"email"}     
+                            name={"mail"} 
+                            value={mail}
+                            onChange={e=>setMail(e.target.value)}
+                            placeholder={"Email Address"} formNoValidate/>
+                    {(mErr != "") && <div className=" flex w-[10%] items-center"><img  src="/images/icon-error.svg" alt="Error Icon" /></div>    }
+                </div>
+                {(mErr == "empty") &&  <div className="italic font-normal text-right pr-5  text-xs text-red-500 " >Email Address cannot be Empty</div>}
+                {(mErr == "valid") &&  <div className="italic font-normal text-right pr-5  text-xs text-red-500 " >Looks like this is not an Email</div>}
+            </div> 
+            <div className="flex flex-col w-full">
+                <div className="my-4 pr-2 flex  flex-nowrap border-2  border-gray-300 h-16 rounded-lg">
+                    <input 
+                            className="w-full h-full text-gray-700 rounded-lg p-3 pl-8 border-0" 
+                            type={"password"}     
+                            name={"password"} 
+                            value={password}
+                            onChange={e=>setPassword(e.target.value)}
+                            placeholder={"Password"} formNoValidate/>
+                    {pErr && <div className=" flex w-[10%] items-center"><img  src="/images/icon-error.svg" alt="Error Icon" /></div>    }
+                </div>
+                {pErr &&  <div className="italic font-normal text-right pr-5 text-xs text-red-500 " >Password cannot be empty</div>}
+            </div>            
+
+            <button className="form-button uppercase text-md text-white h-14 my-2 rounded-lg bg-green-500 p-3 w-full" 
+                    type="button"
+                    onClick={validateFn}>Claim your free trail</button>
+        
         </form>
     );
 }
